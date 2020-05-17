283148dd7ee2981a524d6d8edf6a02e314ff72d7
jasom98 <jasom98@gmail.com>
2020-05-17 15:33:55 -0500

    Implementacion de ecena y sonido

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
 
-public class Player : MonoBehaviour {
-	
+	public class Player : MonoBehaviour {
+
 	public SceneController sceneController;
-	
+
 	public AudioClip Sonido = null;		
+	public AudioClip Winner = null;
 	public float	 Volumen = 1.0f;
 	protected Transform Posicion = null;
 
 	public static int score=0;
-	
+
 	private Rigidbody2D rb;
 	public float speed = 1;
 	// Use this for initialization
@@ -21,7 +22,7 @@ public class Player : MonoBehaviour {
 		score = 0;
 			
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		//GetMouseButtonDown => Touch
@@ -32,20 +33,21 @@ public class Player : MonoBehaviour {
 
 	private void OnCollisionEnter2D(Collision2D collision) {
 	if(Sonido) AudioSource.PlayClipAtPoint(Sonido, Posicion.position, Volumen);
-	
+
 	sceneController.YouLose ();			
 	}
-	
-		private void OnTriggerEnter2D(Collider2D collaider) {
+
+	private void OnTriggerEnter2D(Collider2D collaider) {
 		Debug.Log ("You've win one point"+ score);
 		score=score+1;
 		if (score>6){
 			sceneController.YouWin (); 
+			if(Winner) AudioSource.PlayClipAtPoint(Winner, Posicion.position, Volumen);
 			}
 		
 		
 	}
-	
-	
-	
-}
+
+
+
+	}
---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void btnPlay(){
+		SceneManager.LoadScene(0);
+	}
+
+	public void btnCredits(){
+		SceneManager.LoadScene(1);
+	}
+}
